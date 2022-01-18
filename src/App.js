424d8ae3491e10fab6d7b2be53fb0187b1d2299e
Torgeir Beyer <torgeir.beyer@netlife.com>
2022-01-18 16:35:02 +0100

    Some adjsutments

--- src/App.js
+++ src/App.js
@@ -8,15 +8,17 @@ function App() {
         <div className="flex flex-row flex-wrap py-4">
           <aside className="w-full sm:w-1/3 md:w-1/4 px-2">
             <div className="sticky top-0 p-4 w-full">
-              <nav className="flex flex-col overflow-hidden">
+              <nav className="flex flex-row justify-between sm:flex-col overflow-hidden">
                 <Link to="/">home</Link>
                 <Link to="about">about</Link>
                 <Link to="projects">projects</Link>
               </nav>
             </div>
           </aside>
-          <main role="main" className="w-full sm:w-2/3 md:w-3/4 p-4">
-            <Outlet />
+          <main role="main" className="w-full sm:w-2/3 md:w-3/4 px-2">
+            <div className="content p-4">
+              <Outlet />
+            </div>
           </main>
         </div>
       </div>
