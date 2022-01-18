62ffe3d0282f2b216bd3dd3951c5015f3f136dc0
Torgeir Beyer <torgeir.beyer@netlife.com>
2022-01-18 16:21:58 +0100

    add apiverison

--- src/client.js
+++ src/client.js
@@ -4,4 +4,5 @@ export default sanityClient({
   projectId: "n55qisk2", // find this at manage.sanity.io or in your sanity.json
   dataset: "production", // this is from those question during 'sanity init'
   useCdn: true,
+  apiVersion: "2022-01-17",
 });
