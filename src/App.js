75bc726e9482283381945598876574f0c66fe9c6
Torgeir Beyer <torgeir.beyer@netlife.com>
2022-04-07 19:41:39 +0200

    fix redirect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { useQuery } from "react-query";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { sanity } from "./client";
 import Home from "./components/Home";
 import PageLayout from "./components/PageLayout";
 import About from "./components/About";
-import AllProjects from "./components/AllProjects";
 import Project from "./components/Project";
 import NotFound from "./components/NotFound";
 
 function App() {
+  const query =
+    "*[_type == 'project']|order(date desc){slug{current}, title,date}[0]";
+  const { data: project } = useQuery("$projectQuery", () =>
+    sanity.fetch(query)
+  );
+  if (!project) return <pre>wait a second</pre>;
+  const { slug } = project;
   return (
     <Routes>
       <Route index element={<Home />} />
       <Route path="/" element={<PageLayout />}>
         <Route path="information" element={<About />} />
-        <Route path="work" element={<AllProjects />} />
+        <Route path="work" element={<Navigate replace to={slug.current} />} />
         <Route path="work/:slug" element={<Project />} />
       </Route>
       <Route path="*" element={<NotFound />} />
